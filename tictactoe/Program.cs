343ea3cd40a748ce3ca904d80dcde960e6ba2753
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2023-01-06 01:35:56 -0300

    Feito print do mapa e armazenamento de letra

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -12,14 +12,15 @@ namespace tictactoe
     {
         static string[,] tictactoeTable =
         {
-            {"0" , "0", "0" },
-            {"1" , "1" , "1" },
-            {"2" , "2" , "2"},
+            {"_ " , "_", " _" },
+            {"_ " , "_" , " _" },
+            {"_ " , "_" , " _"},
         };
         static void Main(string[] args)
         {
             Console.WriteLine("TIC TAC TOE!");
             Console.WriteLine("Insert O or X");
+            char userLetter = 'n', computerLetter = 'n';
             bool userAnwserredCorrect = false;
             while (!userAnwserredCorrect)
             {
@@ -28,11 +29,32 @@ namespace tictactoe
                 if (result)
                 {
                     userAnwserredCorrect = true;
+                    switch(userChoice.ToUpper())
+                    {
+                        case "X":
+                            userLetter = 'X';
+                            computerLetter = 'Y';
+                            break;
+                        case"Y":
+                            userLetter = 'Y';
+                            computerLetter = 'X';
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
+            Console.Clear();
+            Console.WriteLine("TIC TAC TOE!");
             showTable();
-
-
+            Console.WriteLine("");
+            Console.Write("Choosed Letter: ");
+            Console.WriteLine(userLetter);
+            Console.WriteLine("");
+            Console.WriteLine("Choose a position and write down the X:");
+            //int userChoiceForX = int.TryParse(Console.ReadLine(), out var resultX);
+            Console.WriteLine("Choose a position and write down the Y:");
+            //int userChoiceForY = int.TryParse(Console.ReadLine(), out var resultY);
 
 
         }
@@ -42,22 +64,11 @@ namespace tictactoe
             for (int i = 0; i < tictactoeTable.GetLength(0);i++) {
                 for (int j = 0; j < tictactoeTable.GetLength(1); j++)
                 {
-                    switch (i)
+                    Console.Write(tictactoeTable[i, j]);
+                    if (j == 2)
                     {
-                        case 0:
-                            Console.Write(tictactoeTable[i, j]);
-                            break;
-                        case 1:
-                            Console.Write(tictactoeTable[i, j]);
-                            break;
-                        case 2:
-                            Console.Write(tictactoeTable[i, j]);
-                            break;
-                        default:
-                            break;
+                        Console.WriteLine("");
                     }
-                    
-
                 }
             }
         }
